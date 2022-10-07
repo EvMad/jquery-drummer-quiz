1643b78b32134129c4b977897b03d5810664946b
GitPit <rifflifter@gmail.com>
2022-10-07 13:59:48 -0400

    moved $(start).on('click') up scope

--- assets/js/script.js
+++ assets/js/script.js
@@ -53,6 +53,47 @@ $(document).ready(function () {
 // $(next).on('click', function(e)
 //   {e.preventDefault();});
 
+$(start).on('click', function () {
+
+  console.log("clicked Start");
+
+
+  $(start).hide();
+
+  var timerEl = $("#countdown");
+
+
+var timeLeft = 60;
+
+// Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+var timeInterval = setInterval(function () {
+  // As long as the `timeLeft` is greater than 1
+  if (timeLeft > 1) {
+    // Set the `textContent` of `timerEl` to show the remaining seconds
+    timerEl.text(timeLeft + ' seconds remaining');
+    // Decrement `timeLeft` by 1
+    timeLeft--;
+  } else if (timeLeft === 1) {
+    // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
+    timerEl.text(timeLeft + ' second remaining');
+    timeLeft--;
+  } else {
+    // Once `timeLeft` gets to 0, set `timerEl` to an empty string
+    timerEl.text('');
+    // Use `clearInterval()` to stop the timer
+    clearInterval(timeInterval);
+    // Call the `displayMessage()` function
+    displayMessage();
+  }
+}, 1000);
+
+
+
+
+  createQuestionEl();
+
+});
+
 
 // render question function
 
@@ -145,46 +186,7 @@ function createQuestionEl() {
   };
 
 
-  $(start).on('click', function () {
 
-    console.log("clicked Start");
-
-
-    $(start).hide();
-
-    var timerEl = $("#countdown");
-
-
-  var timeLeft = 60;
-
-  // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
-  var timeInterval = setInterval(function () {
-    // As long as the `timeLeft` is greater than 1
-    if (timeLeft > 1) {
-      // Set the `textContent` of `timerEl` to show the remaining seconds
-      timerEl.text(timeLeft + ' seconds remaining');
-      // Decrement `timeLeft` by 1
-      timeLeft--;
-    } else if (timeLeft === 1) {
-      // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
-      timerEl.text(timeLeft + ' second remaining');
-      timeLeft--;
-    } else {
-      // Once `timeLeft` gets to 0, set `timerEl` to an empty string
-      timerEl.text('');
-      // Use `clearInterval()` to stop the timer
-      clearInterval(timeInterval);
-      // Call the `displayMessage()` function
-      displayMessage();
-    }
-  }, 1000);
-
- 
-
-
-    createQuestionEl();
-
-  });
 
 
 
